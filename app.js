a239b00823b7c728c3b4c9d89f0517864df41bd5
Mahendra Kumar Raichur <mahendra.raichur@gmail.com>
2020-12-02 15:38:25 +0530

    feat button addn

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log("this is mahi");
+
+// alert("this script works");
+// var username = prompt("type username");
+var btnTranslate = document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click",function clickEventHandler(){
+    // alert("clicked");
+    
+});
