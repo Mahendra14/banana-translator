746d192c5cbd44c63e7d08ba8a770605d62c7ccd
Mahendra Kumar Raichur <mahendra.raichur@gmail.com>
2020-12-03 20:16:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,20 @@
 var txtInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("#output");
+var serverURL = " ";
+
+
+function getTranslationURL(text){
+    return serverURL  + "?" + text;
+}
+
+
 
 function clickEventHandler(){
     // alert("clicked");
     //console.log("input",txtInput.value)
+    
+
     outputDiv.innerText = "mahnasknsknf" + txtInput.value;  
 };
 btnTranslate.addEventListener("click",clickEventHandler);
