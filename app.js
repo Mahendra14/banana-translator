cccdd10ed93e00322146ba43d376d10f84219faf
Mahendra Kumar Raichur <mahendra.raichur@gmail.com>
2020-12-03 21:32:19 +0530

    style added
    Added style to the website in the similar way thats needed.

--- app.js
+++ app.js
@@ -2,23 +2,37 @@
 
 // alert("this script works");
 // var username = prompt("type username");
-var txtInput = document.querySelector("#txt-input");
+var txtInput = document.querySelector("#txt-input1");
 var btnTranslate = document.querySelector("#btn-translate");
-var outputDiv = document.querySelector("#output");
-var serverURL = " ";
+var outputDiv = document.querySelector("#output1");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 
 function getTranslationURL(text){
-    return serverURL  + "?" + text;
+    return serverURL  + "?" +"text=" +text;
 }
 
-
+function errorHandler(error){
+    console.log("error occured",error)
+    alert("something wrong with server plse try again sometime");
+}
 
 function clickEventHandler(){
     // alert("clicked");
     //console.log("input",txtInput.value)
-    
+    var inputText = txtInput.value;
 
-    outputDiv.innerText = "mahnasknsknf" + txtInput.value;  
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            console.log(translatedText)
+        })
+        .catch(errorHandler)
+        //if the server is not up
+        
+
+      
 };
 btnTranslate.addEventListener("click",clickEventHandler);
